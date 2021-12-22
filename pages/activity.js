c9a61e6148683760da2e1a747c055bf7e404529a
Yuko Zheng <yuko.zheng@autodesk.com>
2021-12-22 12:25:58 +0800

    Impl activity reg list

--- pages/activity.js
+++ pages/activity.js
@@ -12,15 +12,23 @@ Page({
         id: null,
         isFullScreen: app.globalData.isFullScreen,
         host: app.globalData.host,
-        activity: null,
         active: null,
         layoutVariables: {
-        }
+        },
+        activity: null,
+        raids: [],
+        bossNames: null,
+        itemSets: null
     },
     onLoad: function(options) {
         this.setData(options);
         wx.$activity = this;
-        this.loadActivity();
+        let self = this;
+        this.loadRaids().then(() => {
+            return self.loadItemSets();
+        }).then(() => {
+            return self.loadActivity();
+        });
         this.switchTab2('reg');
     },
     onUnload: function() {
@@ -35,17 +43,100 @@ Page({
             active: active
         });
     },
+    getBossPassed: function (bossObj) {
+        var passed = 0;
+        for (var i = 0; i < this.data.bosses.length; ++i) {
+            if (bossObj.filter(x => x.Name == this.data.bosses[i]).length) {
+                ++passed;
+            }
+        }
+        return passed;
+    },
+    getWcl: function (bossObj) {
+        var wcl = 0.0;
+        for (var i = 0; i < this.data.bosses.length; ++i) {
+            if (bossObj.filter(x => x.Name == this.data.bosses[i]).length) {
+                wcl += bossObj.filter(x => x.Name == this.data.bosses[i])[0].Parse;
+            }
+        }
+        return wcl / this.data.bosses.length * 1.0;
+    },
+    handleRegistration: function(reg) {
+        if (!reg.charactor) {
+            return;
+        }
+
+        let bossObj = reg.role == 2 
+            ? JSON.parse(reg.charactor.hpsBossRanks) 
+            : JSON.parse(reg.charactor.dpsBossRanks);
+        reg.boss = bossObj;
+        reg.bossPassed = this.getBossPassed(bossObj);
+        reg.wcl = this.getWcl(bossObj);
+    },
+    getBossNames: function (raids) {
+        var splited = raids.split(',').map(x => x.trim());
+        var ret = [];
+        for (var i = 0; i < splited.length; ++i) {
+            var raid = this.data.raids.filter(x => x.id == splited[i]);
+            if (!raid.length) {
+                continue;
+            }
+            raid = raid[0];
+            var splited2 = raid.bossList.split(',').map(x => x.trim());
+            for (var j = 0; j < splited2.length; ++j) {
+                ret.push(splited2[j]);
+            }
+        }
+        return ret;
+    },
+    loadItemSets: function() {
+        let self = this;
+        return qv.get(`${this.data.host}/api/item/set`).then(result => {
+            self.setData({
+                itemSets: result.data.data
+            });
+            return Promise.resolve(true);
+        });
+    },
+    loadRaids: function() {
+        let self = this;
+        return qv.get(`${this.data.host}/api/raid`).then(result => {
+            let raids = result.data.data;
+            self.setData({
+                raids: raids
+            });
+            return Promise.resolve(true);
+        });
+    },
     loadActivity: function() {
         wx.showLoading({
           title: '正在加载活动...',
         })
         let self = this;
         return qv.get(`${this.data.host}/api/activity/${this.data.id}`).then(result => {
-            self.setData({ activity: result.data.data });
+            let activity = result.data.data;
+            wx.setNavigationBarTitle({
+              title: activity.name,
+            })
+
+            self.setData({
+                bosses: self.getBossNames(activity.raids)
+            });
+
+            for (let i = 0; i < activity.registrations.length; ++i) {
+                let reg = activity.registrations[i];
+                self.handleRegistration(reg);
+            }
+
+            self.setData({ 
+                activity: activity
+            });
             wx.hideLoading({});
         });
     },
     onShareAppMessage: function () {
-
+        return {
+            title: this.data.activity.name
+        };
     }
 })
--- pages/activity/reg.js
+++ pages/activity/reg.js
@@ -7,20 +7,155 @@ Component({
         activity: null,
         host: getApp().globalData.host,
         isFullScreen: getApp().globalData.isFullScreen,
-        canBack: false
+        canBack: false,
+        bossAmount: 0,
+        groups: [{
+                id: 0,
+                name: '坦克',
+                img: '/assets/images/tank.jpg'
+            },
+            {
+                id: 2,
+                name: '治疗',
+                img: '/assets/images/healer.jpg'
+            },
+            {
+                id: 1,
+                name: '输出',
+                img: '/assets/images/dps.jpg'
+            },
+            {
+                id: 3,
+                name: '消费',
+                img: '/assets/images/consumer.jpg'
+            }
+        ],
+        statistics: null,
+        grouped: null,
+        itemSets: null
     },
 
     methods: {
-        onBackBtnClicked: function() {
+        onBackBtnClicked: function () {
             wx.navigateBack();
         }
     },
 
     lifetimes: {
-        attached: function() {
-            this.setData({ 
-                activity: this.properties.activity,
-                canBack: !!wx.$guild
+        attached: function () {
+            let activity = this.properties.activity;
+            let bossAmount = wx.$activity.data.bosses.length;
+            let statistics = [];
+            let grouped = [];
+
+            // 处理分组
+            for (let i = 0; i < this.data.groups.length; ++i) {
+                let group = this.data.groups[i];
+                let statisticsItem = {};
+                statisticsItem.submit = activity.registrations.filter(x => x.role == group.id).length;
+                statisticsItem.accepted = activity.registrations.filter(x => x.role == group.id && x.status == 4).length;
+                statisticsItem.standby = activity.registrations.filter(x => x.role == group.id && x.status == 2).length;
+                statistics.push(statisticsItem);
+                let groupedItem = activity.registrations.filter(x => x.role == group.id);
+                grouped.push(groupedItem);
+            }
+
+            for (let i = 0; i < activity.registrations.length; ++i) {
+                let reg = activity.registrations[i];
+
+                // 处理WCL着色
+                if (reg.wcl) {
+                    reg.wcl = parseInt(reg.wcl);
+                    reg._wclCss = 'gray';
+                    if (reg.wcl >= 25) {
+                        reg._wclCss = 'green';
+                    }
+                    if (reg.wcl >= 50) {
+                        reg._wclCss = 'blue';
+                    }
+                    if (reg.wcl >= 75) {
+                        reg._wclCss = 'purple';
+                    }
+                    if (reg.wcl >= 95) {
+                        reg._wclCss = 'orange';
+                    }
+                } else {
+                    reg.wcl = '未知';
+                }
+
+                // 处理装等着色
+                if (reg.boss) {
+                    for (let j = 0; j < reg.boss.length; ++j) {
+                        let boss = reg.boss[j];
+                        let raid = wx.$activity.data.raids.filter(x => x.bossList.indexOf());
+                        if (!raid.length) {
+                            continue;
+                        }
+
+                        raid = raid[0];
+                        let itemLevel = boss.ItemLevel;
+                        if (itemLevel < raid.itemLevelEntrance) {
+                            boss._itemCss = 'gray';
+                        } else if (itemLevel < raid.itemLevelPreference && itemLevel >= raid.itemLevelEntrance) {
+                            boss._itemCss = 'green';
+                        } else if (itemLevel < raid.itemLevelGraduated && itemLevel >= raid.itemLevelPreference) {
+                            boss._itemCss = 'blue';
+                        } else if (itemLevel < raid.itemLevelFarm && itemLevel >= raid.itemLevelGraduated) {
+                            boss._itemCss = 'purple';
+                        } else {
+                            boss._itemCss = 'orange';
+                        }
+                    }
+
+                    let highest = reg.boss.filter(x => x.ItemLevel == reg.charactor.highestItemLevel);
+                    if (highest.length) {
+                        highest = highest[0];
+                        reg.charactor._itemCss = highest._itemCss;
+                    }
+                }
+
+                // 处理套装显示
+                if (reg.charactor) {
+                    reg._equipments = reg.charactor.equipments.split(',').map(x => x.trim());
+                    let sets = wx.$activity.data.itemSets[reg.role.toString()];
+                    for (let j = 0; j < sets.length; ++j) {
+                        try {
+                            reg.setName = sets[j].name;
+                            reg.setCount = 0;
+    
+                            for (let k = 0; k < sets[j].items.length; ++k) {
+                                if (reg._equipments.some(x => x == sets[j].items[k])) {
+                                    ++reg.setCount;
+                                }
+                            }
+    
+                            if (reg.setCount > 0) {
+                                break;
+                            }
+                        } catch (e) {
+                            continue;
+                        }
+                    }
+                }
+
+                // 处理经验着色
+                if (reg.bossPassed == bossAmount) {
+                    reg._bossCss = 'purple';
+                } else if (reg.bossPassed > bossAmount / 2) {
+                    reg._bossCss = 'blue';
+                } else if (reg.bossPassed > 0) {
+                    reg._bossCss = 'green';
+                } else {
+                    reg._bossCss = 'gray';
+                }
+            }
+
+            this.setData({
+                activity: activity,
+                canBack: !!wx.$guild,
+                statistics: statistics,
+                grouped: grouped,
+                bossAmount: bossAmount
             });
         }
     }
